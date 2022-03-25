4f17fd7c08515b57155c6536ee06ed5ce78f716c
zkoteiche <zeina.koteiche@gmail.com>
2022-03-25 15:34:15 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,16 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(
+                opt =>
+                {
+                    opt.AddPolicy("CorsPolicy", policy =>
+                    {
+                        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    }
+                    );
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +53,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
